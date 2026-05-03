45e7b4eb8e11a8145174ecd63096e59984c33f43
Eda Kanimer <edakanimer@Eda-MacBook-Pro.local>
2026-05-03 19:17:51 +0200

    Fix indentation errors

--- finance.py
+++ finance.py
@@ -6,9 +6,10 @@ def simple_interest(principal, rate, years):
     """Calculates simple interest."""
     return principal * rate * years / 100
 
+def compound_interest(principal, rate, years):
+    """Calculates compound interest."""
+    return principal * ((1 + rate/100) ** years) - principal
+
 if __name__ == "__main__":
     print(currency_convert(100, 4.2))
     print(simple_interest(1000, 5, 3))
-    def compound_interest(principal, rate, years):
-    """Calculates compound interest."""
-    return principal * ((1 + rate/100) ** years) - principal
